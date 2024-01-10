9f284eabe11ff83c221b578fb5b7f405f7bf4bbe
Pedro Henrique <pedro00dk@gmail.com>
2024-01-10 10:35:28 -0300

    Add solid-js

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 const base = (0, eval)('process.env.BASE') ?? '/'
 
-export default defineConfig({ base })
+export default defineConfig({ base, plugins: [solidPlugin()] })
